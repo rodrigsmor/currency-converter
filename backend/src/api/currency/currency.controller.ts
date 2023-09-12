c56c524333de13dc87b7add577c59a359a1dc166
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-09-11 23:18:21 -0300

    :sparkles: feat: creates get All currencies route

--- backend/src/api/currency/currency.controller.ts
+++ backend/src/api/currency/currency.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { Language } from '../../utils/decorators';
+import { CurrencyService } from './currency.service';
+import { Currencies } from '../../utils/@types/currencies';
 
-@Controller('currency')
-export class CurrencyController {}
+@Controller('/api/v1/currency')
+export class CurrencyController {
+  constructor(private readonly currencyService: CurrencyService) {}
+
+  @Get('/all')
+  async getAllCurrencies(
+    @Language() lang: string,
+    @Query('currency_code') currency_code?: string,
+  ): Promise<Currencies> {
+    return await this.currencyService.getAllCurrencies(lang, currency_code);
+  }
+}
--- backend/src/api/currency/currency.service.ts
+++ backend/src/api/currency/currency.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Currencies } from '../../utils/@types';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CurrencyValidationService } from '../../common/services';
 
 @Injectable()
-export class CurrencyService {}
+export class CurrencyService {
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly currencyValidation: CurrencyValidationService,
+  ) {}
+
+  private readonly BASE_URL = process.env.EXCHANGE_RATE_BASE_URL;
+  private readonly ACCESS_KEY = process.env.EXCHANGE_RATE_ACCESS_KEY;
+
+  async getAllCurrencies(
+    lang: string,
+    currency_code = 'USD',
+  ): Promise<Currencies> {
+    const isCurrencyCodeValid =
+      await this.currencyValidation.isCurrencyCodeValid(currency_code);
+
+    if (!isCurrencyCodeValid)
+      throw new BadRequestException('Provide a valid currency code');
+
+    try {
+      const response = this.httpService.get(
+        `${this.BASE_URL}/${this.ACCESS_KEY}/${currency_code}`,
+      );
+      console.log(`${this.BASE_URL}/${this.ACCESS_KEY}/${currency_code}`);
+    } catch (error) {
+      console.log(error);
+    }
+
+    return null;
+  }
+}
