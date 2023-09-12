148af2a076716c19f1ead9cfb8553af2d70b4adc
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-09-12 00:43:19 -0300

    :goal_net: feat: allow only known languages

--- backend/src/utils/decorators/get-language.decorator.ts
+++ backend/src/utils/decorators/get-language.decorator.ts
@@ -1,4 +1,13 @@
-import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+import {
+  BadRequestException,
+  ExecutionContext,
+  createParamDecorator,
+} from '@nestjs/common';
+import { TranslationEnum } from '../enums';
+
+function isLanguageValid(language: string): boolean {
+  return Object.values(TranslationEnum).includes(language as TranslationEnum);
+}
 
 export const Language = createParamDecorator(
   (data: unknown, context: ExecutionContext) => {
@@ -6,6 +15,14 @@ export const Language = createParamDecorator(
     const acceptLanguageHeader = request.headers['accept-language'];
     const langQuery = request.query.lang;
 
-    return langQuery || acceptLanguageHeader || 'en';
+    const language = langQuery || acceptLanguageHeader || 'en';
+
+    if (!isLanguageValid(language)) {
+      throw new BadRequestException(
+        'we have no support for the language provided',
+      );
+    }
+
+    return language;
   },
 );
