97f7b613dc9715e66c9514b3f12847a927a988a5
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-09-11 23:14:58 -0300

    :wrench: chore: configurates the main.ts to json requests

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bodyParser: true,
+  });
+
+  app.enableCors();
+  app.use(json({ limit: '100mb' }));
+  app.use(urlencoded({ limit: '100mb', extended: true }));
+
   await app.listen(3000);
 }
 bootstrap();
