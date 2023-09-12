efc82ac11decf8d946ac414b897cc143bb5790a9
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-09-12 00:46:07 -0300

    :recycle: refact: transform Currency type into interface

--- backend/src/utils/@types/currency.d.ts
+++ backend/src/utils/@types/currency.d.ts
@@ -1,7 +1,7 @@
-export type CurrencyType = {
+export interface CurrencyType {
   unit_value: number;
   country_name: string;
   currency_code: string;
   currency_name: string;
   monetary_symbol: string;
-};
+}
