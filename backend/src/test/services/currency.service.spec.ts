2612e9b5c00a8c6888ef845a69b8c11d1659ee6e
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-09-19 22:31:19 -0300

    :white_check_mark: test: configures currency.service test module

--- backend/src/test/services/currency.service.spec.ts
+++ backend/src/test/services/currency.service.spec.ts
@@ -1,18 +1,29 @@
-import { HttpService } from '@nestjs/axios';
+import { HttpModule, HttpService } from '@nestjs/axios';
 import { CurrencyService } from '../../api/currency/currency.service';
 import { Test } from '@nestjs/testing';
+import { CurrencyValidationService } from '../../common/services/currency-validation.service';
 
 describe('CurrencyService', () => {
   let httpService: HttpService;
   let currencyService: CurrencyService;
+  let currencyValidationService: CurrencyValidationService;
 
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
-      imports: [HttpService],
-      providers: [CurrencyService],
+      imports: [HttpModule],
+      providers: [CurrencyService, CurrencyValidationService],
     }).compile();
 
     httpService = moduleRef.get<HttpService>(HttpService);
     currencyService = moduleRef.get<CurrencyService>(CurrencyService);
+    currencyValidationService = moduleRef.get<CurrencyValidationService>(
+      CurrencyValidationService,
+    );
+  });
+
+  it('should be defined', () => {
+    expect(httpService).toBeDefined();
+    expect(currencyService).toBeDefined();
+    expect(currencyValidationService).toBeDefined();
   });
 });
