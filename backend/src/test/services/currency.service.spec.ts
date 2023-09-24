d7c1e066e8ceaecf183a6a547601b1967b8d2ff5
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-09-24 13:36:35 -0300

    :test_tube: test: currency service tests

--- backend/src/test/services/currency.service.spec.ts
+++ backend/src/test/services/currency.service.spec.ts
@@ -10,7 +10,7 @@ import {
 import { AxiosResponse } from 'axios';
 import { Test } from '@nestjs/testing';
 import { HttpModule, HttpService } from '@nestjs/axios';
-import { CountryDto, CurrencyDto } from '../../api/currency/dto';
+import { ConversionDto, CountryDto, CurrencyDto } from '../../api/currency/dto';
 import { CurrencyService } from '../../api/currency/currency.service';
 import { CountriesTranslations } from '../../utils/constants/countries';
 import { CurrencyValidationService } from '../../common/services/currency-validation.service';
@@ -221,11 +221,13 @@ describe('CurrencyService', () => {
       }
     });
 
-    it('should throw BadRequestException if one of the codes provided are not valid', async () => {
+    it('should throw InternalServerError if an error occurs while converting the value', async () => {
       jest
         .spyOn(currencyValidationService, 'areCurrenciesCodesValids')
-        .mockResolvedValueOnce(false);
-      mockHttpService.axiosRef.get.mockResolvedValueOnce(null);
+        .mockResolvedValueOnce(true);
+      mockHttpService.axiosRef.get.mockImplementationOnce(async () =>
+        Promise.reject(''),
+      );
 
       try {
         await currencyService.convertCurrency(
@@ -235,15 +237,67 @@ describe('CurrencyService', () => {
           mock_value,
         );
       } catch (error) {
-        expect(error).toBeInstanceOf(BadRequestException);
+        expect(error).toBeInstanceOf(InternalServerErrorException);
         expect(error.message).toStrictEqual(
-          'The codes provided are not valids.',
+          'An error occurred while converting currencies',
         );
         expect(
           currencyValidationService.areCurrenciesCodesValids,
         ).toBeCalledWith([base_country, target_country]);
-        expect(mockHttpService.axiosRef.get).not.toBeCalled();
+        expect(mockHttpService.axiosRef.get).toBeCalledWith(
+          `/pair/${base_country}/${target_country}/${mock_value}`,
+        );
       }
+    });
+
+    it('should convert the currencies and return the result and unit value', async () => {
+      const mockExchangeRateResponse: IExchangeRateResponse = {
+        base_code: base_country,
+        target_code: target_country,
+        documentation: null,
+        result: null,
+        terms_of_use: null,
+        time_last_update_unix: null,
+        time_last_update_utc: null,
+        time_next_update_unix: null,
+        time_next_update_utc: null,
+        conversion_rate: 4.93,
+        conversion_result: 986.98,
+      };
+
+      const axiosResponse: AxiosResponse<IExchangeRateResponse> = {
+        config: null,
+        data: mockExchangeRateResponse,
+        headers: null,
+        status: 200,
+        statusText: 'OK',
+        request: null,
+      };
+
+      jest
+        .spyOn(currencyValidationService, 'areCurrenciesCodesValids')
+        .mockResolvedValueOnce(true);
+      mockHttpService.axiosRef.get.mockImplementationOnce(() => axiosResponse);
+
+      const result = await currencyService.convertCurrency(
+        'en',
+        base_country,
+        target_country,
+        mock_value,
+      );
+
+      expect(result).toStrictEqual(
+        new ConversionDto(
+          mockExchangeRateResponse.conversion_result,
+          mockExchangeRateResponse.conversion_rate,
+        ),
+      );
+      expect(currencyValidationService.areCurrenciesCodesValids).toBeCalledWith(
+        [base_country, target_country],
+      );
+      expect(mockHttpService.axiosRef.get).toBeCalledWith(
+        `/pair/${base_country}/${target_country}/${mock_value}`,
+      );
     });
   });
 });
