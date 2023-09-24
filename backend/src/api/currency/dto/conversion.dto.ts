f34a96a77b9ca4785160b353db2609a53132c69d
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-09-24 00:13:47 -0300

    :label: feat: creates constructor to conversionDto

--- backend/src/api/currency/dto/conversion.dto.ts
+++ backend/src/api/currency/dto/conversion.dto.ts
@@ -1,4 +1,9 @@
 export class ConversionDto {
   result: number;
   unit_value: number;
+
+  constructor(result: number, unit_value: number) {
+    this.result = result;
+    this.unit_value = unit_value;
+  }
 }
