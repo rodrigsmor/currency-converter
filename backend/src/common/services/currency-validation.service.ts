7d49674b3238bdfb4d23a56929df28a8b4fd8ada
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-09-24 00:12:56 -0300

    :sparkles: feat: are currencies codes valids service

--- backend/src/common/services/currency-validation.service.ts
+++ backend/src/common/services/currency-validation.service.ts
@@ -6,4 +6,8 @@ export class CurrencyValidationService {
   async isCurrencyCodeValid(currency: string): Promise<boolean> {
     return Object.values(CurrenciesEnum).includes(currency as CurrenciesEnum);
   }
+
+  async areCurrenciesCodesValids(currencies: string[]): Promise<boolean> {
+    return currencies.every((currency) => this.isCurrencyCodeValid(currency));
+  }
 }
