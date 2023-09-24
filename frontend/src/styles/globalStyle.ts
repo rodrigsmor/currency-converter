e4b57d399db3fa03c7c5f3f36714ca7386b786e0
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-09-24 20:03:01 -0300

    :lipstick: feat: set the project font family

--- frontend/src/styles/globalStyle.ts
+++ frontend/src/styles/globalStyle.ts
@@ -1,10 +1,13 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700;800&display=swap');
+
  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: 'Manrope', sans-serif;
   }
 
   html {
