629205b461ca2bd15ce21c118d395e49d9ff4723
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-09-26 07:24:02 -0300

    :sparkles: feat: develops Logo component

---
+++ frontend/src/components/common/logo/styled.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const SvgContainer = styled.svg`
+  width: 40px;
+  height: 40px;
+
+  > path {
+    fill: ${(props) => props?.theme?.colors?.background };
+  }
+
+  &.dark {
+    > path {
+      fill: ${(props) => `${props?.theme?.colors?.typography}${props?.theme?.opacities?.[80]}` };
+    }
+  }
+
+  @media ${(props) => props?.theme?.breakpoints?.large } {
+    width: 46px;
+    height: 46px;
+  }
+`;
