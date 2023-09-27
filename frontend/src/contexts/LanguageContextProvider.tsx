ccb802c37a5b0727f332b006ed872d4b93d2c0ff
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-09-27 07:32:37 -0300

    :sparkles: feat: create Language context

---
+++ frontend/src/contexts/LanguageContextProvider.tsx
@@ -0,0 +1,41 @@
+import { LangEnum } from '@/utils/enums/lang.enum';
+import { PropsWithChildren, createContext, useEffect, useState } from 'react';
+
+interface LanguageContextProvider {
+  lang: LangEnum;
+  setLang: (lang: LangEnum) => void;
+}
+
+export const LanguageContext = createContext<LanguageContextProvider>({
+  lang: LangEnum.en,
+  setLang: () => {}
+});
+
+export function LanguageContextProvider({ children }: PropsWithChildren) {
+  const [ lang, setLang ] = useState<LangEnum>(() => {
+    let langStorage = null;
+
+    if (typeof window !== 'undefined') {
+      langStorage = localStorage.getItem('lang@currency-converter');
+    }
+
+    return langStorage 
+      ? JSON.parse(langStorage) as LangEnum
+      : LangEnum.en
+  });
+
+  useEffect(() => {
+    localStorage.setItem('lang@currency-converter', JSON.stringify(lang));
+  }, [lang])
+
+  return (
+    <LanguageContext.Provider
+      value={{
+        lang,
+        setLang,
+      }}
+    >
+      { children }
+    </LanguageContext.Provider>
+  )
+}
