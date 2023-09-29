c5ff08097cc86645676d9a0a9e5d384ccb555c81
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-09-28 21:15:27 -0300

    :sparkles: feat: close LangOptionsList when select an option

--- frontend/src/components/forms/langSelect/index.tsx
+++ frontend/src/components/forms/langSelect/index.tsx
@@ -49,7 +49,7 @@ export function LangSelect() {
                 role='option'
                 aria-selected={isSelected}
               >
-                <button onClick={e => setLang(langOption?.lang)}>
+                <button disabled={isSelected} onClick={() => [setLang(langOption?.lang), setIsSelectExpanded(false)]}>
                   <div aria-label={`${langOption?.label} flag miniature`} className={`fi fi-${langOption?.countryCode} fis country-flag-lang`}></div>
                   <span
                     className='option-language_CODE'
