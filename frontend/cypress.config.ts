91f640fb02e7135d224a356b948c4b942e09f8d2
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-09-29 21:04:33 -0300

    :wrench: chore: adds spec path to cypress config

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   component: {
+    specPattern: 'tests/components/**/*.cy.{js,jsx,ts,tsx}',
     devServer: {
       framework: 'next',
       bundler: 'webpack',
