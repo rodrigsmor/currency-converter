69ca4446ee47e17cfdd1c82a3cc739203e97b0b5
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-10-02 21:00:59 -0300

    :white_check_mark: test: option selecting in lang select

--- frontend/tests/components/forms/langSelect.spec.cy.tsx
+++ frontend/tests/components/forms/langSelect.spec.cy.tsx
@@ -13,8 +13,23 @@ describe('<LangSelect />', () => {
   it('should only renders the LangOptionsList when click in LangSelectToggle', () => {
     cy.get('ul#Lang-Select_List').should('have.attr', 'aria-hidden', 'true');
     cy.get('ul#Lang-Select_List').should('have.css', 'opacity', '0');
+
     cy.get('button#Lang-Select_Toggle').click();
+
     cy.get('ul#Lang-Select_List').should('have.attr', 'aria-hidden', 'false');
     cy.get('ul#Lang-Select_List').should('have.css', 'opacity', '1');
   })
+
+  it('should select the language correctly', () => {
+    cy.get('span#language-selected_code').should('contain.text', 'EN');
+    cy.get('li#option-EN').should('have.attr', 'aria-selected', 'true');
+    cy.get('li#option-PT').should('have.attr', 'aria-selected', 'false');
+
+    cy.get('button#Lang-Select_Toggle').click();
+    cy.get('li#option-PT>button').click();
+
+    cy.get('span#language-selected_code').should('contain.text', 'PT');
+    cy.get('li#option-EN').should('have.attr', 'aria-selected', 'false');
+    cy.get('li#option-PT').should('have.attr', 'aria-selected', 'true');
+  });
 });
