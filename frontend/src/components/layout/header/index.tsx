8f85e8a3b98c42e34b56e931fb63cf69221a5368
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-10-07 23:12:11 -0300

    :construction: feat: adds MenuSidebar to header

--- frontend/src/components/layout/header/index.tsx
+++ frontend/src/components/layout/header/index.tsx
@@ -1,17 +1,27 @@
 'use client'
 
+import { useState } from 'react';
 import { usePathname } from 'next/navigation';
+
+// components
+import { MenuSidebar } from '../menuSidebar';
 import { Logo } from '@/components/common/logo';
 import { navRoutes } from '@/utils/constants/nav-routes';
 import { Searchbar } from '@/components/forms/searchbar';
-import Menu4FillIcon from 'remixicon-react/Menu4FillIcon';
 import { LangSelect } from '@/components/forms/langSelect';
 import { IconButton } from '@/components/buttons/IconButton';
 import { ThemeSwitcher } from '@/components/forms/themeSwitcher';
+
+// icons
+import Menu4FillIcon from 'remixicon-react/Menu4FillIcon';
+
+// styles
 import { HeaderContainer, HeaderNav, HeaderNavOption, InteractionsWrapper } from './styled';
 
 export const Header = () => {
   const pathname = usePathname();
+
+  const [showSidebar, setSidebar] = useState<boolean>(false);
 
   return (
     <HeaderContainer>
@@ -44,6 +54,7 @@ export const Header = () => {
         <Searchbar color='background' />
       </InteractionsWrapper>
       <IconButton color='transparent' Icon={<Menu4FillIcon size={32} />} />
+      <MenuSidebar showSidebar={showSidebar} toggleSidebar={setSidebar} />
     </HeaderContainer>
   );
 }
