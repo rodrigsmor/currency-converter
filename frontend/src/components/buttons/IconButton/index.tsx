3a177b807bccc2811d35ea9854987e7cdb501dc9
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-10-09 00:00:06 -0300

    :sparkles: feat: adds label to IconButton

--- frontend/src/components/buttons/IconButton/index.tsx
+++ frontend/src/components/buttons/IconButton/index.tsx
@@ -4,15 +4,22 @@ import { MouseEventHandler } from 'react';
 import { IconButtonContainer } from './styled'
 
 interface IconButtonProps {
+  label: string;
   Icon: JSX.Element;
   color?: 'transparent' | 'background';
   onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
-export const IconButton = ({ color = 'background', Icon, onClick }: IconButtonProps) => {
+export const IconButton = ({
+  color = 'background',
+  Icon,
+  onClick,
+  label,
+}: IconButtonProps) => {
   return (
     <IconButtonContainer
       onClick={onClick}
+      aria-label={label}
       className={`icon-button ${color}`}
     >
       { Icon }
