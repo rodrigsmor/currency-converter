77d4552a75f5fdf92fe01d274b5b44a2990c98b5
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-10-10 22:43:32 -0300

    :lipstick: feat: style the home page container

---
+++ frontend/src/styles/common/styled.ts
@@ -0,0 +1,13 @@
+'use client'
+
+import styled from 'styled-components';
+
+export const PageContainer = styled.div`
+  display: flex;
+  width: 100dvw;
+  max-width: 100dvw;
+  overflow-x: hidden;
+  position: relative;
+  min-height: 100dvh;
+  flex-direction: column;
+`;
