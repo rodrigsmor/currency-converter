3e6625666f10b47b4ccf8a7b1f84c5b52f28ba82
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-10-10 22:43:59 -0300

    :sparkles: feat: adds the home page content

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,21 @@
+import { Metadata } from 'next';
+
+// components
 import { Header } from '@/components/layout/header';
+
+// style
+import { PageContainer } from '@/styles/common/styled';
+import { HomePageContent } from '@/components/pages/home';
+
+export const metadata: Metadata = {
+  title: 'Home | Currency Converter',
+}
 
 export default function Home() {
   return (
-    <main>
+    <PageContainer>
       <Header />
-      sasassasasasas
-    </main>
+      <HomePageContent />
+    </PageContainer>
   )
 }
