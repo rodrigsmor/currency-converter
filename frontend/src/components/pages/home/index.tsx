bc778585cc7b2d24a4b517b9e1ab800ebf99ac68
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-10-11 20:38:31 -0300

    :sparkles: feat: creates Currency converted data

--- frontend/src/components/pages/home/index.tsx
+++ frontend/src/components/pages/home/index.tsx
@@ -1,13 +1,26 @@
 'use client'
 
-import { HomePageMainContainer, TopGreetingsHeader } from './styled'
+import { CurrencyPreviewBox, CurrencyValudeConverted, HomePageMainContainer, TopGreetingsHeader } from './styled'
 
 export const HomePageContent = () => {
   return (
     <HomePageMainContainer>
       <TopGreetingsHeader>
-        <h1>Welcome back!</h1>
-        <h2>Check the euro today</h2>
+        <hgroup>
+          <h1>Welcome back!</h1>
+          <h2>Check the euro today</h2>
+        </hgroup>
+        <CurrencyPreviewBox>
+          <p>
+            1 United States Dollar equals
+            <strong className='ValueConverted_Container'>
+              <CurrencyValudeConverted>
+                <small className='ValueConverted_MonetarySymbol'>€</small> 1.07
+              </CurrencyValudeConverted>
+              <span className='ValueConverted_CurrencyName'>Euro</span>
+            </strong>
+          </p>
+        </CurrencyPreviewBox>
       </TopGreetingsHeader>
     </HomePageMainContainer>
   )
