c1dfbbfb458757c203ca6ea801b7d8140834be90
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-10-12 15:47:36 -0300

    :sparkles: feat: creates currencies and countries mocks

---
+++ frontend/src/utils/mocks/countries.ts
@@ -0,0 +1,10 @@
+import { Country } from '../@types/country';
+
+export const countries_mock: Country[] = [
+  {
+    flag_code: 'eu',
+    currency_code: 'EUR',
+    monetary_symbol: '',
+    country_name: 'European Union',
+  }
+];
