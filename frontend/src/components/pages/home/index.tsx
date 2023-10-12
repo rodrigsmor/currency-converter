a7399d1ebf6d3df7c3db2f6da03120da96148ef9
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-10-11 21:28:21 -0300

    :sparkles: feat: adds CurrencySelector to Home page

--- frontend/src/components/pages/home/index.tsx
+++ frontend/src/components/pages/home/index.tsx
@@ -1,8 +1,29 @@
 'use client'
 
+import { MouseEvent, useState } from 'react'
+
+// icons
+import More2LineIcon from 'remixicon-react/More2LineIcon';
+
+// buttons
+import { IconButton } from '@/components/buttons/IconButton'
+import { CurrencySelector } from '@/components/forms/currencySelector'
+
+// styles
 import { CurrencyPreviewBox, CurrencyValudeConverted, HomePageMainContainer, TopGreetingsHeader } from './styled'
 
 export const HomePageContent = () => {
+  const [showCurrencySelector, setShowCurrencySelector] = useState<boolean>(false);
+
+  function handleToggleCurrencySelector(event: MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+    setShowCurrencySelector(!showCurrencySelector)
+  }
+
+  function handleSelectOption(event: MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+  }
+
   return (
     <HomePageMainContainer>
       <TopGreetingsHeader>
@@ -20,6 +41,16 @@ export const HomePageContent = () => {
               <span className='ValueConverted_CurrencyName'>Euro</span>
             </strong>
           </p>
+          <IconButton
+            Icon={<More2LineIcon />}
+            color='background-20'
+            onClick={handleToggleCurrencySelector}
+            label={`${showCurrencySelector ? 'Open' : 'Close'} currency selector`}
+          />
+          <CurrencySelector
+            showCurrencySelector={showCurrencySelector}
+            onSelectOption={handleSelectOption}
+          />
         </CurrencyPreviewBox>
       </TopGreetingsHeader>
     </HomePageMainContainer>
