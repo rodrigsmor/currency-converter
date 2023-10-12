699ec433294662f1ef5e5795d61caf03e383aa51
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-10-11 22:08:22 -0300

    :sparkles: feat: adds aria-label to searchbar

--- frontend/src/components/forms/searchbar/index.tsx
+++ frontend/src/components/forms/searchbar/index.tsx
@@ -6,14 +6,19 @@ import SearchLineIcon from 'remixicon-react/SearchLineIcon';
 interface SearchbarProps {
   color?: 'primary' | 'background';
   placeholder?: string;
+  label?: string;
 }
 
 export const Searchbar = ({
   color = 'primary',
-  placeholder = 'Search'
+  placeholder = 'Search',
+  label,
 }: SearchbarProps) => {
   return (
-    <SearchbarContainer className={`searchbar-container ${color}`}>
+    <SearchbarContainer
+      className={`searchbar-container ${color}`}
+      {...(label && { "aria-label": label })}
+    >
       <SearchLineIcon size={24} />
       <SearchInput
         placeholder={placeholder}
