697471063b802ded3f5bdb393cba060b15220a70
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-10-16 19:46:12 -0300

    :sparkles: feat: sets initial state and push to selected route

--- frontend/src/contexts/LanguageContextProvider.tsx
+++ frontend/src/contexts/LanguageContextProvider.tsx
@@ -1,8 +1,15 @@
 'use client'
 
-import { Language, langList } from '@/utils/constants/lang-list';
+import { PropsWithChildren, createContext, useState } from 'react';
+
+// enums
 import { LangEnum } from '@/utils/enums/lang.enum';
-import { PropsWithChildren, createContext, useEffect, useState } from 'react';
+
+// constants
+import { Language, langList } from '@/utils/constants/lang-list';
+
+// hooks
+import { useParams, usePathname, useRouter } from 'next/navigation';
 
 interface LanguageContextProvider {
   lang: LangEnum;
@@ -12,33 +19,27 @@ interface LanguageContextProvider {
 
 export const LanguageContext = createContext<LanguageContextProvider>({
   lang: LangEnum.en,
-  setLang: () => {},
+  setLang: (lang: LangEnum) => {},
   currentLanguage: langList.find((lang) => lang.lang === LangEnum.en),
 });
 
 export function LanguageContextProvider({ children }: PropsWithChildren) {
+  const params = useParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
   const [ lang, setLang ] = useState<LangEnum>(() => {
-    // let langStorage = null;
-
-    // if (typeof window !== 'undefined') {
-    //   langStorage = localStorage.getItem('lang@currency-converter');
-    // }
-
-    // return langStorage
-    //   ? JSON.parse(langStorage) as LangEnum
-    //   : LangEnum.en
-    return LangEnum.en
+    return params.lang ? params.lang as LangEnum : LangEnum.en
   });
-
-  useEffect(() => {
-    localStorage.setItem('lang@currency-converter', JSON.stringify(lang));
-  }, [lang])
 
   return (
     <LanguageContext.Provider
       value={{
         lang,
-        setLang,
+        setLang: (lang) => {
+          const newPath = pathname.replace(/\/[^/]+/, `/${lang}`);
+          router.push(newPath, { scroll: false })
+        },
         currentLanguage: langList.find((langItem) => langItem.lang === lang as LangEnum),
       }}
     >
