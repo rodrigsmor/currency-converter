cf46cc7b07741aef68d6e62a31c885ce5e2f4a7c
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-10-16 21:30:41 -0300

    :label: feat: creates the dictionary instances

---
+++ frontend/src/i18n/dictionaries/en.ts
@@ -0,0 +1,5 @@
+export default {
+  'home.greetings': 'Welcome back!',
+  'home.baseCurrencyCheck': 'Check the {currency_name} today.',
+  'home.currencyConverted.equivalence': '1 {currency_name} is equals to',
+} as const;
---
+++ frontend/src/i18n/dictionaries/pt-BR.ts
@@ -0,0 +1,5 @@
+export default {
+  'home.greetings': 'Bem-vindo de volta!',
+  'home.baseCurrencyCheck': 'Dê uma olhada no {currency_name} hoje.',
+  'home.currencyConverted.equivalence': '1 {currency_name} é igual à',
+} as const;
