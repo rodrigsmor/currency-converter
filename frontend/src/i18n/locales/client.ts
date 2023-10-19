c994648b0feddccc18af613ad93b5cbc49a85fa0
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-10-18 22:34:56 -0300

    :sparkles: feat: exports change and current locale from client

--- frontend/src/i18n/locales/client.ts
+++ frontend/src/i18n/locales/client.ts
@@ -1,7 +1,8 @@
-"use client"
+'use client'
+
 import { createI18nClient } from 'next-international/client'
 
-export const { useI18n, useScopedI18n, I18nProviderClient } = createI18nClient({
+export const { useI18n, useScopedI18n, I18nProviderClient, useChangeLocale, useCurrentLocale } = createI18nClient({
   en: () => import('../dictionaries/en'),
   'pt-BR': () => import('../dictionaries/pt-BR')
 })
