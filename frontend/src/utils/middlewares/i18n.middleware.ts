4442f4ad136e0f483fca18ede53640c6949a2682
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-10-18 22:36:06 -0300

    :ok_hand: style: organize content of middleware

--- frontend/src/utils/middlewares/i18n.middleware.ts
+++ frontend/src/utils/middlewares/i18n.middleware.ts
@@ -1,6 +1,5 @@
-// middleware.ts
-import { createI18nMiddleware } from 'next-international/middleware'
 import { NextRequest } from 'next/server'
+import { createI18nMiddleware } from 'next-international/middleware'
 
 const I18nMiddleware = createI18nMiddleware({
   locales: ['en', 'pt-BR'],
