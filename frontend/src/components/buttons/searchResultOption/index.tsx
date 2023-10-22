161b4d453745d947b869dadb1e0f70d9201c90e5
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-10-21 23:18:43 -0300

    :sparkles: feat: renders Link or Button depending on isRecent

--- frontend/src/components/buttons/searchResultOption/index.tsx
+++ frontend/src/components/buttons/searchResultOption/index.tsx
@@ -1,16 +1,44 @@
 'use client'
 
+import { useContext } from 'react';
+
+// i18n
 import { useI18n } from '@/i18n/locales/client';
+
+// Icons
 import CloseLineIcon from 'remixicon-react/CloseLineIcon';
+
+// types
 import { RecentSearches } from '@/utils/@types/recent-searches';
-import { SearchResultButtonWrap, SearchResultDetails, SearchResultWrapper } from './styled';
+
+// contexts
+import { LanguageContext } from '@/contexts/LanguageContextProvider';
+
+// components
+import { SearchResultButtonWrap, SearchResultDetails, SearchResultLinkWrap, SearchResultWrapper } from './styled';
 
 interface SearchResultOptionProps {
   search: RecentSearches;
+  isRecent: boolean;
 }
 
-export function SearchResultOption({ search }: SearchResultOptionProps) {
+function SearchDetailsContent({ search }: { search: RecentSearches }) {
   const t = useI18n()
+
+  return (
+    <>
+      <span aria-label={`${search.value} flag`} className={`fi fi-${search?.country_flag} fis country-flag-resultButton`}></span>
+      <SearchResultDetails>
+        <p className='ResultOption_DetailsValue'>{search.value}</p>
+        <span className='ResultOption_TypeSelected'>{t(search.type_label)}</span>
+      </SearchResultDetails>
+    </>
+  );
+}
+
+export function SearchResultOption({ search, isRecent }: SearchResultOptionProps) {
+  const t = useI18n()
+  const { lang } = useContext(LanguageContext)
 
   function removeRecentSearches() {
     // remove 
@@ -18,21 +46,33 @@ export function SearchResultOption({ search }: SearchResultOptionProps) {
 
   return (
     <SearchResultWrapper>
-      <SearchResultButtonWrap>
-        <span aria-label={`${search.value} flag`} className={`fi fi-${search?.country_flag} fis country-flag-resultButton`}></span>
-        <SearchResultDetails>
-          <p className='ResultOption_DetailsValue'>{search.value}</p>
-          <span className='ResultOption_TypeSelected'>{t(search.type_label)}</span>
-        </SearchResultDetails>
-      </SearchResultButtonWrap>
-      <button
-        aria-label={t('search.result.remove')}
-        title={t('search.result.remove')}
-        onClick={() => removeRecentSearches()}
-        className='RemoveRecentSearch'
-      >
-        <CloseLineIcon size={20} />
-      </button>
+      {
+        isRecent
+          ? (
+            <SearchResultLinkWrap
+              tabIndex={0}
+              href={`/${lang}/${search.path}?target_currency=${search.currencyCode}`}
+            >
+              <SearchDetailsContent search={search} />
+            </SearchResultLinkWrap>
+          ) : (
+            <SearchResultButtonWrap>
+              <SearchDetailsContent search={search} />
+            </SearchResultButtonWrap>
+          )
+      }
+      {
+        isRecent && (
+          <button
+            aria-label={t('search.result.remove')}
+            title={t('search.result.remove')}
+            onClick={() => removeRecentSearches()}
+            className='RemoveRecentSearch'
+          >
+            <CloseLineIcon size={20} />
+          </button>
+        )
+      }
     </SearchResultWrapper>
   )
 }
