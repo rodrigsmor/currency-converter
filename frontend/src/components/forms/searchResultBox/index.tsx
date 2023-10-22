1ba0009dde29d6c908703eccaf364a679e175058
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-10-22 19:35:15 -0300

    :sparkles: feat: adds search matches results listing

--- frontend/src/components/forms/searchResultBox/index.tsx
+++ frontend/src/components/forms/searchResultBox/index.tsx
@@ -1,26 +1,31 @@
 'use client'
 
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 
 // i18n
 import { useI18n } from '@/i18n/locales/client'
 
 // utils
+import { currencies_mock } from '@/utils/mocks/currencies'
 import { recent_searches_mock } from '@/utils/mocks/recent-searches'
+
+// types
+import { Currency } from '@/utils/@types/currency'
+import { FeatureSearchOption } from '@/utils/@types/feature-search-option'
 
 // components
 import { Searchbar } from '../searchbar'
 import { FeatureSearchSelect } from '../featureSearchSelect'
 import { SearchResultOption } from '@/components/buttons/searchResultOption'
 
-// styles
-import { SearchResultBoxWrapper, SearchResultSection, SearchResultsDropbox } from './styled'
-
 // constants
 import { searchResultsFeatures } from '@/utils/constants/search-results-features'
 
-// types
-import { FeatureSearchOption } from '@/utils/@types/feature-search-option'
+// styles
+import { SearchResultBoxWrapper, SearchResultSection, SearchResultsDropbox } from './styled'
+
+// icons
+import SearchLineIcon from 'remixicon-react/SearchLineIcon'
 
 interface SearchResultBoxProps {
   
@@ -32,6 +37,18 @@ export const SearchResultBox = ({  }: SearchResultBoxProps) => {
   const [searchValue, setSearchValue] = useState<string>('');
   const [featureSelected, setFeatureSelected] = useState<FeatureSearchOption | null>(null);
 
+  const searchMatches = useMemo<Currency[]>(() => {
+    const currenciesItems = [...currencies_mock];
+    let searchQuery = searchValue.toLowerCase();
+
+    return currenciesItems.filter((currency) => (
+      currency.flag_code.toLowerCase().includes(searchQuery) ||
+      currency.currency_name.toLowerCase().includes(searchQuery) ||
+      currency.currency_code.toLowerCase().includes(searchQuery) ||
+      currency.country_name.toLowerCase().includes(searchQuery)
+    ));
+  }, [ searchValue ])
+
   return (
     <SearchResultBoxWrapper
       className='search-result-box'
@@ -41,24 +58,57 @@ export const SearchResultBox = ({  }: SearchResultBoxProps) => {
           id='SearchResultBox_Searchbar'
           color='background-full'
           value={searchValue}
+          placeholder={t('searchbar.currency')}
           onChange={event => setSearchValue(event.target.value)}
         />
       </header>
       <SearchResultsDropbox>
-        <SearchResultSection>
-          <h3 className='SearchResult_SectionTitle'>{t('search.result.recentSection')}</h3>
-          <ul className='SearchResult_List'>
-            {
-              recent_searches_mock.map((search, index) => {
-                return (
-                  <li key={index}>
-                    <SearchResultOption isRecent={true} search={search} />
-                  </li>
-                )
-              })
-            }
-          </ul>
-        </SearchResultSection>
+        {
+          (searchValue?.length > 0) ? (
+            <SearchResultSection key='SearchsMatches_Results'>
+              <h3 className='SearchResult_SectionTitle'>{t('search.result.recentSection')}</h3>
+              <ul className='SearchResult_List'>
+                {
+                  searchMatches.length > 0 ? (
+                    searchMatches.map((currency, index) => {
+                      return (
+                        <li key={index}>
+                          <SearchResultOption isRecent={false} search={{
+                            country_flag: currency.flag_code,
+                            currencyCode: currency.currency_code,
+                            path: featureSelected?.path || 'convert-currencies',
+                            type_label: featureSelected?.optionName || 'header.converter',
+                            value: currency.currency_name,
+                          }} />
+                        </li>
+                      )
+                    })
+                  ) : (
+                    <li className='SearchResult_EmptyState'>
+                      <SearchLineIcon size={48} className='currency-selector-empty' />
+                      <p>{t('search.result.empty')} <strong>&ldquo;{searchValue}&rdquo;</strong>.</p>
+                    </li>
+                  )
+                }
+              </ul>
+            </SearchResultSection>
+          ) : (
+            <SearchResultSection key='RecentSearches_Results'>
+              <h3 className='SearchResult_SectionTitle'>{t('search.result.results')}</h3>
+              <ul className='SearchResult_List'>
+                {
+                  recent_searches_mock.map((search, index) => {
+                    return (
+                      <li key={index}>
+                        <SearchResultOption isRecent={true} search={search} />
+                      </li>
+                    )
+                  })
+                }
+              </ul>
+            </SearchResultSection>
+          )
+        }
         <SearchResultSection>
           <h3 className='SearchResult_SectionTitle'>{t('search.result.feature')}</h3>
           <div className='SearchResult_Features'>
