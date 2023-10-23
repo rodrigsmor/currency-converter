5e8c18d57d6469c805c12942cd6f9b9cd6b76cf8
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-10-22 21:23:19 -0300

    :lipstick: feat: styles Search Options in SearchResultBox styled component

--- frontend/src/components/forms/searchResultBox/styled.ts
+++ frontend/src/components/forms/searchResultBox/styled.ts
@@ -51,29 +51,101 @@ export const SearchResultSection = styled.section`
     overflow-y: auto;
     flex-direction: column;
 
-    > li.SearchResult_EmptyState {
-      gap: 4px;
-      width: 100%;
-      display: grid;
-      max-width: 100%;
-      overflow: hidden;
-      min-height: 104px;
-      place-items: center;
-      color: ${(props) => `${props?.theme?.colors?.typography + props?.theme?.opacities?.[80]}`};
-
-      > p {
+    > li {
+      > div.SearchResult_OptionItem {
+        gap: 10px;
         width: 100%;
-        text-align: center;
-        word-break: break-all;
-        overflow-wrap: break-word;
-        max-height: 100%;
-        font-size: ${(props) => props?.theme?.typography?.sizes?.h6};
-        font-weight: ${(props) => props?.theme?.typography?.weight?.medium};
-        color: ${(props) => `${props?.theme?.colors?.typography + props?.theme?.opacities?.[60]}`};
+        height: 32px;
+        display: flex;
+        max-width: 100%;
+        cursor: pointer;
+        padding: 4px 8px;
+        border-radius: 8px;
+        align-items: center;
+        flex-direction: row;
+        align-items: center;
+        transition: all .5s ease-out;
+        background: ${(props) => props?.theme?.colors?.background};
 
-        > strong {
-          font-weight: ${(props) => props?.theme?.typography?.weight?.semibold};
-          color: ${(props) => `${props?.theme?.colors?.typography + props?.theme?.opacities?.[80]}`};
+        &:hover {
+          opacity: .6;
+          background: ${(props) => props?.theme?.colors?.typography + props?.theme?.opacities?.[20]};
+        }
+
+        > .RemoveRecentSearch {
+          width: 24px;
+          height: 24px;
+          display: grid;
+          cursor: pointer;
+          place-items: center;
+          border: 0px solid transparent;
+          background-color: transparent;
+          color: ${(props) => props?.theme?.colors?.typography + props?.theme?.opacities?.[60]};
+        }
+
+        > .ResultOption_ContentWrapper {
+          gap: 10px;
+          width: 100%;
+          display: flex;
+          cursor: pointer;
+          max-width: 100%;
+          align-items: center;
+          text-decoration: none;
+          border: 0px solid transparent;
+          background-color: transparent;
+          justify-content: space-between;
+
+          > .country-flag-resultButton {
+            width: 20px;
+            height: 20px;
+            border-radius: 6px;
+          }
+
+          > .SearchResultOption_Details {
+            gap: 4px;
+            flex-grow: 1;
+            display: flex;
+            align-items: center;
+
+            > p.ResultOption_DetailsValue {
+              font-size: ${(props) => props?.theme?.typography?.sizes?.h6};
+              font-weight: ${(props) => props?.theme?.typography?.weight?.bold};
+              color: ${(props) => props?.theme?.colors?.typography + props?.theme?.opacities?.[80]};
+            }
+
+            > span.ResultOption_TypeSelected {
+              font-size: 12px;
+              font-weight: ${(props) => props?.theme?.typography?.weight?.regular};
+              color: ${(props) => props?.theme?.colors?.typography + props?.theme?.opacities?.[40]};
+            }
+          }
+        }
+      }
+
+      &.SearchResult_EmptyState {
+        gap: 4px;
+        width: 100%;
+        display: grid;
+        max-width: 100%;
+        overflow: hidden;
+        min-height: 104px;
+        place-items: center;
+        color: ${(props) => `${props?.theme?.colors?.typography + props?.theme?.opacities?.[80]}`};
+  
+        > p {
+          width: 100%;
+          text-align: center;
+          word-break: break-all;
+          overflow-wrap: break-word;
+          max-height: 100%;
+          font-size: ${(props) => props?.theme?.typography?.sizes?.h6};
+          font-weight: ${(props) => props?.theme?.typography?.weight?.medium};
+          color: ${(props) => `${props?.theme?.colors?.typography + props?.theme?.opacities?.[60]}`};
+  
+          > strong {
+            font-weight: ${(props) => props?.theme?.typography?.weight?.semibold};
+            color: ${(props) => `${props?.theme?.colors?.typography + props?.theme?.opacities?.[80]}`};
+          }
         }
       }
     }
